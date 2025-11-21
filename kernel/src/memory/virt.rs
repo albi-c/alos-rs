40e5d21de4f614f0ec5cefa3c9a021c8cb1847f2
albi-c <albert.csontos@gmail.com>
2025-11-21 14:09:55 +0100

    work on virtual memory deallocation

--- kernel/src/memory/virt.rs
+++ kernel/src/memory/virt.rs
@@ -1,11 +1,54 @@
 use alloc::collections::BTreeMap;
 use core::ops::Bound;
 use crate::memory::address::{PageCount, VirtAddr, VirtAddrPageAligned};
+use crate::println;
 
 #[derive(Debug, Copy, Clone)]
 struct MemoryAllocation {
     allocated: bool,
     length: PageCount,
+}
+
+#[derive(Debug, Copy, Clone)]
+enum AddrIntersection {
+    None,
+    All,
+    PartialDown(VirtAddrPageAligned),
+    PartialUp(VirtAddrPageAligned),
+    PartialMid(BaseWithLength),
+}
+
+#[derive(Debug, Copy, Clone)]
+struct BaseWithLength(VirtAddrPageAligned, PageCount);
+
+impl BaseWithLength {
+    pub fn start(self) -> VirtAddrPageAligned {
+        self.0
+    }
+
+    pub fn length(self) -> PageCount {
+        self.1
+    }
+
+    pub fn end(self) -> VirtAddrPageAligned {
+        self.0 + self.1
+    }
+
+    pub fn intersect(self, other: BaseWithLength) -> AddrIntersection {
+        if self.start() > other.end() || self.end() <= other.start() {
+            AddrIntersection::None
+        } else if self.start() >= other.start() && self.end() <= other.end() {
+            AddrIntersection::All
+        } else if self.start() < other.start() && self.end() > other.end() {
+            AddrIntersection::PartialMid(other)
+        } else if self.start() < other.start() {
+            AddrIntersection::PartialUp(other.start())
+        } else if self.end() > other.end() {
+            AddrIntersection::PartialDown(other.end())
+        } else {
+            unreachable!()
+        }
+    }
 }
 
 impl MemoryAllocation {
@@ -81,15 +124,59 @@ impl VirtualMemorySpace {
         }
     }
     
-    pub fn deallocate(&mut self, base: VirtAddrPageAligned, size: PageCount) {
+    pub fn deallocate(&mut self, start: VirtAddrPageAligned, size: PageCount) {
         // TODO: callback for physical memory deallocation
-        let end = base + size;
-        let mut cur = self.data.lower_bound_mut(Bound::Included(&base));
+        let base_with_length = BaseWithLength(start, size);
+        println!("deallocating {:x?} +{:x?}", start, size);
+        let end = base_with_length.end();
+        let mut cur = self.data.lower_bound_mut(Bound::Unbounded);
         while let Some((&base, entry)) = cur.next() {
+            if !entry.allocated {
+                continue;
+            }
+            let entry_end = base + entry.length;
+            if entry_end <= start {
+                continue;
+            }
             if base >= end {
                 break;
             }
-            todo!()
+            println!("deallocate {:x?} +{:x?} {:x?}", base, entry.length, BaseWithLength(base, entry.length).intersect(base_with_length));
+            match BaseWithLength(base, entry.length).intersect(base_with_length) {
+                AddrIntersection::None => {},
+                AddrIntersection::All => {
+                    entry.allocated = false;
+                    let entry = *entry;
+                    cur.prev().unwrap();
+                    if let Some(prev) = cur.peek_prev() {
+                        if !prev.1.allocated {
+                            prev.1.length = prev.1.length + entry.length;
+                            cur.remove_next().unwrap();
+                        } else {
+                            cur.next().unwrap();
+                        }
+                    }
+                    if let Some(next) = cur.peek_next() {
+                        if !next.1.allocated {
+                            let length = next.1.length;
+                            cur.remove_next().unwrap();
+                            let prev = cur.peek_prev().unwrap();
+                            prev.1.length = prev.1.length + length;
+                        }
+                    }
+                },
+                AddrIntersection::PartialMid(bwl) => {
+                    todo!()
+                },
+                AddrIntersection::PartialUp(up) => {
+                    todo!()
+                },
+                AddrIntersection::PartialDown(down) => {
+                    todo!()
+                },
+            }
         }
+
+        println!("{:#x?}", self.data);
     }
 }
